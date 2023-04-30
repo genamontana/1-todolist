e51bbb508c3c9b976dc952e765f77592c9bade98
genamontana <predator199@yandex.ru>
2023-05-01 01:20:05 +0300

    12 lesson in process 1 hour

---
+++ src/stories/todolists-api.stories.tsx
@@ -0,0 +1,39 @@
+import React, {useEffect, useState} from 'react'
+
+export default {
+    title: 'API'
+}
+
+export const GetTodolists = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+
+
+    }, [])
+    return <div>{JSON.stringify(state)}</div>
+}
+
+export const CreateTodolist = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+    }, [])
+
+    return <div>{JSON.stringify(state)}</div>
+}
+
+export const DeleteTodolist = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+    }, [])
+
+    return <div>{JSON.stringify(state)}</div>
+}
+
+export const UpdateTodolistTitle = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+    }, [])
+
+    return <div>{JSON.stringify(state)}</div>
+}
+
